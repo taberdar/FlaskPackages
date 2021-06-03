fa90b50bc58a2ee83de9240408e024e766d5a1df
Andrew Chisholm <andrew.chisholm@hsdc.ac.uk>
2021-06-03 07:20:41 +0000

    add code

--- FlaskPackages/app.py
+++ FlaskPackages/app.py
@@ -4,6 +4,8 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask
+import importlib
+
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -14,6 +16,16 @@ wsgi_app = app.wsgi_app
 def hello():
     """Renders a sample page."""
     return "Hello World!"
+
+@app.route('/packages')
+def packages():
+    packages = ['pandas', 'matplotlib', 'numpy', 'pygame', 'flask', 'django', 'requests', 'scipy']
+    for package in packages:
+	    try:
+		    var = importlib.import_module(package)
+		    print('Package ' + package + ' OK' + '</br>')
+	    except ModuleNotFoundError:
+		    print('Package ' + package + ' not found' + '</br>')
 
 if __name__ == '__main__':
     import os
